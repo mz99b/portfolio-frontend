b58e40250695ee6097732e03236fe15ee887cb78
mz99b <mxtthieu.b@outlook.fr>
2025-06-16 03:01:18 +0200

    Fix: Escape entities in HeroSection to comply with ESLint rules

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -4,9 +4,9 @@ const HeroSection = () => {
   return (
     <section id="hero" className={styles.heroContainer}>
       <h1>Matthieu Burger</h1>
-      <h2>Concepteur d'Application Web & Développeur Full-Stack</h2>
+      <h2>Concepteur d&apos;Application Web & Développeur Full-Stack</h2>
       <p>
-        Développeur indépendant, très motivé afin d'intégrer et d'apprendre au sein d'une entreprise. Curieux et intéressé, je saurais être à l'écoute des missions qu'on me propose afin de progresser et de faire avancer l'entreprise !
+        Développeur indépendant, très motivé afin d&apos;intégrer et d&apos;apprendre au sein d&apos;une entreprise. Curieux et intéressé, je saurais être à l&apos;écoute des missions qu&apos;on me propose afin de progresser et de faire avancer l&apos;entreprise !
       </p>
     </section>
   );
